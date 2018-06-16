c9915a123e9dc28f987736c4e3a2dfc0fb9ad84b
johrad <jpdragic@gmail.com>
2018-06-16 16:07:13 +0200

    added click to run functionality

--- first.js
+++ first.js
@@ -1,11 +1,16 @@
-while (true) {
-    var num = Number(prompt("gimme a number nibba"));
-    if (!Number.isNaN(num)){
-        alert("Your number is the square root of " + num * num);
-        break    
-    }
-    else {
-        alert("give me a number! Not a fucking string >:( \nTry again")
-    }
+// while (true) {
+//     var num = Number(prompt("gimme a number nibba"));
+//     if (!Number.isNaN(num)){
+//         alert("Your number is the square root of " + num * num);
+//         break
+//     }
+//     else {
+//         alert("give me a number! Not a fucking string >:( \nTry again")
+//     }
+// }
+// console.log("Ye boi")
+//
+
+function JavaSript_run(){
+  alert("Fuck you faggot");
 }
-console.log("Ye boi")
