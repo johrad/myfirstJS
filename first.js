5b1ea18d1945999e96a16490af1bb370d91d3055
johrad <jpdragic@gmail.com>
2018-06-17 15:45:38 +0200

    added second button and hover animation

--- first.js
+++ first.js
@@ -1,27 +1,5 @@
-// while (true) {
-//     var num = Number(prompt("gimme a number nibba"));
-//     if (!Number.isNaN(num)){
-//         alert("Your number is the square root of " + num * num);
-//         break
-//     }
-//     else {
-//         alert("give me a number! Not a fucking string >:( \nTry again")
-//     }
-// }
-// console.log("Ye boi")
-//
-
-<<<<<<< HEAD
-function JavaSript_run(){
-  alert("Fuck you!");
-=======
+// insert cool comment here
 function JavaScript_run(){
-  var name = prompt("what is your name brotha?");
-  if(name=="jp"){
-    alert("Your name is fucking lit boi")
-  }else(){
-    alert("Your name sucks balls!")
-  }
->>>>>>> ad4b2e94e9304ff1e95480b1644fe8b372a6277f
+  alert("Oh, hello there");
+  alert("Obi-Wan Kenobi")
 }
-JavaScript_run()
