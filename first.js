c6ef701757424d970de2ad7138bcab2bd4739015
johrad <jpdragic@gmail.com>
2018-06-21 21:11:07 +0200

    formating

--- first.js
+++ first.js
@@ -1,8 +1,14 @@
 // insert cool comment here
-function Load_log(){
+function Load_log() {
   console.log("Website loaded successfully")
 }
 
-function JavaScript_run(){
-  alert("sup")
+var chicks = new Array("Kate upton", "Emiliy ratatatawoski", "Demi Lovato",
+  "Jennifer Aniston");
+var bois = new Array("Bob", "Ken", "Carl");
+var people = chicks.concat(bois);
+
+function JavaScript_run() {
+  alert(people[5]);
+  console.log("end")
 }
